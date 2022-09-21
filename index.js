08c04ffa0153b15300cc830b2ef8501c4e2bc983
caesarkutaa <caesarlouis0@gmail.com>
2022-09-21 16:25:06 +0100

    cont coding for testimonals

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-//set initial count
-let count = 0
-
-const value = document.querySelector('#value')
-const btns = document.querySelectorAll('.btn')
-
- btns.forEach(function(btn){
-    btn.addEventListener("click", function(e){
-         const counter = e.currentTarget.classList;
-         if(counter.contains("decrease")){
-            count--;
-         } else if (counter.contains("reset")){
-            count = 0;
-         }else{
-           count++;
-         }
-
-         if(count > 0){
-            value.style.color ='green';
-         } else if(count <  0){
-           value.style.color = 'red'
-         }else{
-            value.style.color = 'black'
-         }
-         value.textContent = count
-    })
-
- })
-
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+  
