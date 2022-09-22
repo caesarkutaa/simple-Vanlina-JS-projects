77c86bac9a49ef0978e040974dddf9b9487b7f5d
caesarkutaa <caesarlouis0@gmail.com>
2022-09-22 11:56:09 +0100

    finished hero list

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+ 
+ let newhero = []
+
+ const inputEL =document.getElementById("text-el")
+ const inputBtn = document.getElementById("input-btn")
+ const ulEL = document.getElementById("ul-el")
+ const deleteBtn = document.getElementById("delete-btn")
+
+ 
+
+ function render(){
+    let listitems = ""
+    for(let i = 0; i < newhero.length; i++){
+        listitems += "<li> <center>" + newhero[i] +"</center></li>"
+        }
+    ulEL.innerHTML = listitems
+
+}
+  deleteBtn.addEventListener("click", function(){
+ 
+     newhero = []
+      render()
+  })  
+
+ inputBtn.addEventListener("click", function(){
+  newhero.push(inputEL.value)
+   inputEL.value = ""
+   
+
+
+   render()
+    
+ })
+  
+inputEL.addEventListener("keypress", function onEvent(event){
+    if (event.key === "Enter"){
+        document.getElementById("input-btn").click();
+    }
+})
