21e96a043c94f067988c0bd5b8ddabc3e408f795
caesarkutaa <caesarlouis0@gmail.com>
2022-09-26 14:57:56 +0100

    Added arry filter hero list

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
  
- let newhero = []
+ let newhero = ["wonder womman", "batman"]
 
  const inputEL =document.getElementById("text-el")
  const inputBtn = document.getElementById("input-btn")
  const ulEL = document.getElementById("ul-el")
  const deleteBtn = document.getElementById("delete-btn")
-
+ const delBtn = document.getElementsByClassName("del-btn")
  
 
  function render(){
     let listitems = ""
     for(let i = 0; i < newhero.length; i++){
-        listitems += "<li> <center>" + newhero[i] +"</center></li>"
+        listitems += '<li class="flex-me"><span class="mr-2">' + newhero[i] +'</span> <span class="ml-2"><button onClick="deleteHero('+i+')">delete</button></span></li>'
+
         }
     ulEL.innerHTML = listitems
 
 }
   deleteBtn.addEventListener("click", function(){
  
-     newhero = []
+    newhero = []
+      
+  
       render()
   })  
 
- inputBtn.addEventListener("click", function(){
-  newhero.push(inputEL.value)
-   inputEL.value = ""
-   
 
-
-   render()
-    
+// add a new list from clicking the input button
+inputBtn.addEventListener("click", function(){
+    let trimmedVal = inputEL.value.trim()
+    if(trimmedVal.length < 1)
+        return
+    newhero.push(trimmedVal)
+    inputEL.value = ""
+    render()
  })
   
+
+// add a new list on clicking the keyboard's "Enter" key 
 inputEL.addEventListener("keypress", function onEvent(event){
     if (event.key === "Enter"){
         document.getElementById("input-btn").click();
     }
 })
+
+
+function deleteHero(index) {
+    let elemToRemove = newhero[index]
+    newhero = newhero.filter(function(value) {
+        return value != elemToRemove
+    })
+    render()
+}
