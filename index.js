d9c86e0b1b814e3be68cc2d45c6f0bfab12043c4
caesarkutaa <caesarlouis0@gmail.com>
2022-10-17 02:33:29 +0100

    qr code styling:

--- index.js
+++ index.js
@@ -1,62 +1,11 @@
-  let btn = document.querySelector('#new-quote')
-  let quote = document.querySelector('.quote')
-  let person = document.querySelector('.person')
+const wrapper = document.querySelector(".wrapper")
+qrInput = wrapper.querySelector(".form input")
+generateBtn = wrapper.querySelector(".form button")
 
-  const quotes = [
-    {
-      person: "susan smith",
-     quote:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    { 
-      person: "anna johnson",
-      quote:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      person: "peter jones",
-      quote:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      person: "bill anderson",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      person: "bill anderson",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      person: "ndibe augsutine",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      person: "stephen orakwe",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      person: "caesar kuta",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      person: "brenda okeke",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      person: " king kuta",
-      quote:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-  ];
 
-  btn.addEventListener("click", function(){
-    let random = Math.floor(Math.random() * quotes.length);
-    quote.innerText = quotes[random].quote;
-    person.innerText =quotes[random].person;
-  })
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value;
+  if(!qrValue) return; // prevent adding active clase in the wrapper if the input is empty
+  console.log(qrValue);
+  wrapper.classList.add("active")
+}) 
